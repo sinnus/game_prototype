15f697bfbf4b1c98132e65ade79fa91cf03bebb4
Kirill Trofimov <sinnus@gmail.com>
2009-11-22 21:52:07 +0300

    Added is_authorized mockup into logon resourse

--- src/game_prototype_login_resource.erl
+++ src/game_prototype_login_resource.erl
@@ -2,8 +2,13 @@
 %% @copyright YYYY author.
 %% @doc Example webmachine_resource.
 -module(game_prototype_login_resource).
-
+-export([is_authorized/2]).
 -include_lib("request_html.hrl").
+
+is_authorized(ReqData, Context) ->
+    ReqData1 = wrq:append_to_response_body(<<"Access denied">>, ReqData),
+    ReqData2 = wrq:set_resp_header("Content-Type", "text/html; charset=utf-8", ReqData1),
+    {"HelloWorld", ReqData2, Context}.
 
 html(ReqData, Context) ->
     Ssid = binary_to_list(Context#http_context.ssid),
