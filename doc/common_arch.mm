a401b367dc55661286dcdaed50150ed219ee1f7d
Kirill Trofimov <sinnus@gmail.com>
2010-03-17 22:48:54 +0300

    Initial commit of common_arch mindmap

---
+++ doc/common_arch.mm
@@ -0,0 +1,11 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#2189dd" CREATED="1268855189567" ID="Freemind_Link_573658800" MODIFIED="1268855240678" TEXT="&#x410;&#x440;&#x445;&#x438;&#x442;&#x435;&#x43a;&#x442;&#x443;&#x440;&#x430;">
+<node CREATED="1268855243069" ID="_" MODIFIED="1268855251606" POSITION="right" TEXT="&#x418;&#x433;&#x440;&#x43e;&#x43a;"/>
+<node CREATED="1268855254922" ID="Freemind_Link_1891668417" MODIFIED="1268855257562" POSITION="left" TEXT="&#x418;&#x433;&#x440;&#x43e;&#x432;&#x43e;&#x439; &#x43c;&#x438;&#x440;"/>
+<node CREATED="1268855260685" ID="Freemind_Link_1005280592" MODIFIED="1268855264925" POSITION="right" TEXT="&#x418;&#x433;&#x440;&#x43e;&#x432;&#x44b;&#x435; &#x441;&#x43e;&#x431;&#x44b;&#x442;&#x438;&#x44f;"/>
+<node CREATED="1268855266251" ID="Freemind_Link_1200301310" MODIFIED="1268855273922" POSITION="left" TEXT="&#x411;&#x414;"/>
+<node CREATED="1268855278685" ID="Freemind_Link_1710680905" MODIFIED="1268855281122" POSITION="right" TEXT="&#x41a;&#x43e;&#x43d;&#x444;&#x438;&#x433;&#x443;&#x440;&#x430;&#x446;&#x438;&#x44f;"/>
+<node CREATED="1268855282221" ID="Freemind_Link_1992827951" MODIFIED="1268855283979" POSITION="left" TEXT="&#x420;&#x435;&#x441;&#x443;&#x440;&#x441;&#x44b;"/>
+</node>
+</map>
