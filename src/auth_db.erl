42f3279b06d6edd5b70b72d983add786925d1817
Kirill Trofimov <sinnus@gmail.com>
2010-03-22 00:16:06 +0300

    Added create_account fun

--- src/auth_db.erl
+++ src/auth_db.erl
@@ -4,7 +4,7 @@
 
 -record(account, {id,
 		  account_ref_id, % Reference to external account storage
-		  type,           % type (internal, vkontakte, facebook)
+		  type,           % type (0 - internal, 1 - vkontakte, 2 - facebook)
 		  enabled}).      % true / false
 
 %%%----------------------------------------------------------------------
@@ -19,24 +19,32 @@ start() ->
 %% Result: created
 %%         exists
 try_create_account(AccountRefId, Type) ->
-    F = fun() ->
-		case mnesia:index_read(account, AccountRefId, #account.account_ref_id) of
-		    [] ->
-			create_account(AccountRefId, Type);
-		    [Accounts] ->
-			case lists:filter(fun(Elem)  -> 
-						  Elem#account.type == Type
-					  end, Accounts) of
-			    [] ->
-				create_account(AccountRefId, Type);
-			    [_] ->
-				%% TODO: log warning if length(Accounts) > 1
-				exists
-			end
-		end
-	end,
+F = fun() ->
+	    %% Maybe we should use dirty_index_read???
+	    case mnesia:index_read(account, AccountRefId, #account.account_ref_id) of
+		[] ->
+		    create_account(AccountRefId, Type);
+		Accounts ->
+		    case lists:filter(fun(Elem)  ->
+					      Elem#account.type =:= Type
+				      end, Accounts) of
+			[] ->
+			    create_account(AccountRefId, Type);
+			[_] ->
+			    %% TODO: log warning if length(Accounts) > 1
+			    exists
+		    end
+	    end
+    end,
     {atomic, Result} = mnesia:transaction(F),
     Result.
 
-create_account(_AccountRefId, _Type) ->
+create_account(AccountRefId, Type) ->
+    Seq = sequence_server:get_sequence(account),
+    F = fun() -> mnesia:write(#account{id = Seq,
+				       account_ref_id = AccountRefId,
+				       type = Type,
+				       enabled = true})
+	end,
+    {atomic, _Result} = mnesia:transaction(F),
     created.
