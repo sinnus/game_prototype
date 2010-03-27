fb56d58ca4bca7acfa42652f42fc2dfbdc12ecc3
sinnus <sinnus@desktop.(none)>
2010-03-27 14:53:33 +0300

    Added http_session mindmap

---
+++ doc/http_session.mm
@@ -0,0 +1,28 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1269645277475" ID="Freemind_Link_1352651459" MODIFIED="1269688844077" TEXT="Zotonic">
+<node CREATED="1269645689948" ID="_" MODIFIED="1269645697983" POSITION="right" TEXT="z_session_manager">
+<node CREATED="1269645711136" ID="Freemind_Link_346412265" MODIFIED="1269645719518" TEXT="&#x421;&#x43e;&#x437;&#x434;&#x430;&#x435;&#x442; &#x434;&#x43b;&#x44f; &#x43d;&#x43e;&#x432;&#x43e;&#x433;&#x43e; &#x43a;&#x43e;&#x43d;&#x43d;&#x435;&#x43a;&#x442;&#x430; z_session"/>
+<node CREATED="1269645737762" ID="Freemind_Link_467864345" MODIFIED="1269645746981" TEXT="&#x421;&#x432;&#x44f;&#x437;&#x44b;&#x432;&#x430;&#x435;&#x442; sid -&gt; pid"/>
+<node CREATED="1269645751069" ID="Freemind_Link_512736850" MODIFIED="1269645755358" TEXT="&#x41c;&#x43e;&#x43d;&#x438;&#x442;&#x43e;&#x440;&#x438;&#x442; &#x441;&#x435;&#x441;&#x441;&#x438;&#x44e;"/>
+<node CREATED="1269689735781" ID="Freemind_Link_1418755360" MODIFIED="1269689737985" TEXT="expire timer"/>
+</node>
+<node CREATED="1269645700293" ID="Freemind_Link_696526265" MODIFIED="1269645704415" POSITION="left" TEXT="z_session">
+<node CREATED="1269645862596" ID="Freemind_Link_457492362" MODIFIED="1269645869456" TEXT="&#x425;&#x440;&#x430;&#x43d;&#x438;&#x442; &#x43f;&#x435;&#x440;&#x435;&#x43c;&#x435;&#x43d;&#x43d;&#x44b;&#x435; &#x432; &#x441;&#x435;&#x441;&#x441;&#x438;&#x438;"/>
+<node CREATED="1269645894396" ID="Freemind_Link_866631925" MODIFIED="1269645952104" TEXT="manager &#x432;&#x44b;&#x437;&#x44b;&#x432;&#x430;&#x435;&#x442; check_expire"/>
+<node CREATED="1269646017462" ID="Freemind_Link_1176560693" MODIFIED="1269646027129" TEXT="&#x44d;&#x442;&#x43e; gen_server"/>
+</node>
+<node CREATED="1269688718953" ID="Freemind_Link_327597831" MODIFIED="1269688721372" POSITION="right" TEXT="z_auth">
+<node CREATED="1269688726109" ID="Freemind_Link_780040942" MODIFIED="1269688729672" TEXT="logon_pw"/>
+<node CREATED="1269688732901" ID="Freemind_Link_608625851" MODIFIED="1269688739591" TEXT="&#x43f;&#x440;&#x43e;&#x432;&#x435;&#x440;&#x43a;&#x430; &#x432;&#x430;&#x43b;&#x438;&#x434;&#x43d;&#x43e;&#x441;&#x442;&#x438; &#x43b;&#x43e;&#x433;&#x438;&#x43d; - &#x43f;&#x430;&#x440;&#x43e;&#x43b;&#x44c;"/>
+<node CREATED="1269688740795" ID="Freemind_Link_1383300326" MODIFIED="1269688757838" TEXT="z_auth:logon">
+<node CREATED="1269690646151" ID="Freemind_Link_1690128862" MODIFIED="1269690654482" TEXT="&#x414;&#x43e;&#x441;&#x442;&#x430;&#x435;&#x442; Roles"/>
+<node CREATED="1269690655616" ID="Freemind_Link_1497292158" MODIFIED="1269690667505" TEXT="&#x421;&#x43e;&#x437;&#x434;&#x430;&#x435;&#x442; #acl &#x438; &#x43a;&#x43b;&#x430;&#x434;&#x435;&#x442; &#x432; &#x43a;&#x43e;&#x43d;&#x442;&#x435;&#x43a;&#x441;&#x442;"/>
+</node>
+<node CREATED="1269688759530" ID="Freemind_Link_590918151" MODIFIED="1269688788387" TEXT="z_session_manager:rename_session"/>
+<node CREATED="1269688789863" ID="Freemind_Link_272693080" MODIFIED="1269688805977" TEXT="z_context:set_session">
+<node CREATED="1269688818447" ID="Freemind_Link_1919504592" MODIFIED="1269688832630" TEXT="cast(Pid)"/>
+</node>
+</node>
+</node>
+</map>
