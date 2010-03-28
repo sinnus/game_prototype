bd61dba9d0f8173619f6d76f8c2c270b26ee29b3
sinnus <sinnus@desktop.(none)>
2010-03-29 01:00:58 +0400

    Changed http_session.mm

--- doc/http_session.mm
+++ doc/http_session.mm
@@ -1,18 +1,20 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1269645277475" ID="Freemind_Link_1352651459" MODIFIED="1269688844077" TEXT="Zotonic">
-<node CREATED="1269645689948" ID="_" MODIFIED="1269645697983" POSITION="right" TEXT="z_session_manager">
+<node CREATED="1269645277475" ID="Freemind_Link_1352651459" MODIFIED="1269765350546" TEXT="HttpSession">
+<node CREATED="1269765351697" ID="Freemind_Link_1799409152" MODIFIED="1269765885483" POSITION="left" TEXT="Zotonic">
+<node CREATED="1269645700293" ID="Freemind_Link_696526265" MODIFIED="1269765522912" TEXT="z_session">
+<node CREATED="1269645862596" ID="Freemind_Link_457492362" MODIFIED="1269645869456" TEXT="&#x425;&#x440;&#x430;&#x43d;&#x438;&#x442; &#x43f;&#x435;&#x440;&#x435;&#x43c;&#x435;&#x43d;&#x43d;&#x44b;&#x435; &#x432; &#x441;&#x435;&#x441;&#x441;&#x438;&#x438;"/>
+<node CREATED="1269645894396" ID="Freemind_Link_866631925" MODIFIED="1269645952104" TEXT="manager &#x432;&#x44b;&#x437;&#x44b;&#x432;&#x430;&#x435;&#x442; check_expire"/>
+<node CREATED="1269646017462" ID="Freemind_Link_1176560693" MODIFIED="1269646027129" TEXT="&#x44d;&#x442;&#x43e; gen_server"/>
+</node>
+<node CREATED="1269645689948" ID="_" MODIFIED="1269765522914" TEXT="z_session_manager">
+<arrowlink DESTINATION="Freemind_Link_696526265" ENDARROW="Default" ENDINCLINATION="101;0;" ID="Freemind_Arrow_Link_1826511478" STARTARROW="None" STARTINCLINATION="101;0;"/>
 <node CREATED="1269645711136" ID="Freemind_Link_346412265" MODIFIED="1269645719518" TEXT="&#x421;&#x43e;&#x437;&#x434;&#x430;&#x435;&#x442; &#x434;&#x43b;&#x44f; &#x43d;&#x43e;&#x432;&#x43e;&#x433;&#x43e; &#x43a;&#x43e;&#x43d;&#x43d;&#x435;&#x43a;&#x442;&#x430; z_session"/>
 <node CREATED="1269645737762" ID="Freemind_Link_467864345" MODIFIED="1269645746981" TEXT="&#x421;&#x432;&#x44f;&#x437;&#x44b;&#x432;&#x430;&#x435;&#x442; sid -&gt; pid"/>
 <node CREATED="1269645751069" ID="Freemind_Link_512736850" MODIFIED="1269645755358" TEXT="&#x41c;&#x43e;&#x43d;&#x438;&#x442;&#x43e;&#x440;&#x438;&#x442; &#x441;&#x435;&#x441;&#x441;&#x438;&#x44e;"/>
 <node CREATED="1269689735781" ID="Freemind_Link_1418755360" MODIFIED="1269689737985" TEXT="expire timer"/>
 </node>
-<node CREATED="1269645700293" ID="Freemind_Link_696526265" MODIFIED="1269645704415" POSITION="left" TEXT="z_session">
-<node CREATED="1269645862596" ID="Freemind_Link_457492362" MODIFIED="1269645869456" TEXT="&#x425;&#x440;&#x430;&#x43d;&#x438;&#x442; &#x43f;&#x435;&#x440;&#x435;&#x43c;&#x435;&#x43d;&#x43d;&#x44b;&#x435; &#x432; &#x441;&#x435;&#x441;&#x441;&#x438;&#x438;"/>
-<node CREATED="1269645894396" ID="Freemind_Link_866631925" MODIFIED="1269645952104" TEXT="manager &#x432;&#x44b;&#x437;&#x44b;&#x432;&#x430;&#x435;&#x442; check_expire"/>
-<node CREATED="1269646017462" ID="Freemind_Link_1176560693" MODIFIED="1269646027129" TEXT="&#x44d;&#x442;&#x43e; gen_server"/>
-</node>
-<node CREATED="1269688718953" ID="Freemind_Link_327597831" MODIFIED="1269688721372" POSITION="right" TEXT="z_auth">
+<node CREATED="1269688718953" ID="Freemind_Link_327597831" MODIFIED="1269765518480" TEXT="z_auth">
 <node CREATED="1269688726109" ID="Freemind_Link_780040942" MODIFIED="1269688729672" TEXT="logon_pw"/>
 <node CREATED="1269688732901" ID="Freemind_Link_608625851" MODIFIED="1269688739591" TEXT="&#x43f;&#x440;&#x43e;&#x432;&#x435;&#x440;&#x43a;&#x430; &#x432;&#x430;&#x43b;&#x438;&#x434;&#x43d;&#x43e;&#x441;&#x442;&#x438; &#x43b;&#x43e;&#x433;&#x438;&#x43d; - &#x43f;&#x430;&#x440;&#x43e;&#x43b;&#x44c;"/>
 <node CREATED="1269688740795" ID="Freemind_Link_1383300326" MODIFIED="1269688757838" TEXT="z_auth:logon">
@@ -22,6 +24,60 @@
 <node CREATED="1269688759530" ID="Freemind_Link_590918151" MODIFIED="1269688788387" TEXT="z_session_manager:rename_session"/>
 <node CREATED="1269688789863" ID="Freemind_Link_272693080" MODIFIED="1269688805977" TEXT="z_context:set_session">
 <node CREATED="1269688818447" ID="Freemind_Link_1919504592" MODIFIED="1269688832630" TEXT="cast(Pid)"/>
+</node>
+</node>
+<node CREATED="1269765665179" ID="Freemind_Link_1453255157" MODIFIED="1269765666975" TEXT="rpc">
+<node CREATED="1269765668091" ID="Freemind_Link_148158439" MODIFIED="1269765675382" TEXT="&#x412;&#x44b;&#x437;&#x43e;&#x432;&#x44b; &#x438;&#x434;&#x443;&#x442; &#x432; &#x43e;&#x431;&#x445;&#x43e;&#x434; z_session"/>
+<node CREATED="1269765676410" ID="Freemind_Link_622040338" MODIFIED="1269765683678" TEXT="z_session - &#x442;&#x43e;&#x43b;&#x44c;&#x43a;&#x43e; &#x43a;&#x430;&#x43a; &#x43f;&#x435;&#x440;&#x441;&#x438;&#x441;&#x442;&#x435;&#x43d;&#x441;"/>
+</node>
+</node>
+<node CREATED="1269765360508" ID="Freemind_Link_447890185" MODIFIED="1269765896699" POSITION="right" TEXT="Prototype">
+<node CREATED="1269765779005" ID="Freemind_Link_1041951528" MODIFIED="1269765782673" TEXT="&#x422;&#x440;&#x435;&#x431;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x44f;">
+<node CREATED="1269765786909" ID="Freemind_Link_588521105" MODIFIED="1269765807383" TEXT="&#x412;&#x435;&#x441;&#x442;&#x438; &#x441;&#x43f;&#x438;&#x441;&#x43e;&#x43a;, &#x43a;&#x442;&#x43e; &#x43e;&#x43d;&#x43b;&#x430;&#x439;&#x43d;"/>
+<node CREATED="1269765814235" ID="Freemind_Link_886876206" MODIFIED="1269765835470" TEXT="&#x420;&#x430;&#x437;&#x43b;&#x43e;&#x433;&#x438;&#x43d;&#x438;&#x432;&#x430;&#x442;&#x44c; &#x434;&#x443;&#x431;&#x43b;&#x438;&#x440;&#x443;&#x44e;&#x449;&#x443;&#x44e; &#x441;&#x435;&#x441;&#x441;&#x438;&#x44e;"/>
+</node>
+<node CREATED="1269765851539" ID="Freemind_Link_754201397" MODIFIED="1269765885482" TEXT="&#x420;&#x435;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f;">
+<node CREATED="1269767334633" ID="Freemind_Link_1617948760" MODIFIED="1269767343908" TEXT="http_session: gen_server"/>
+<node CREATED="1269767345397" ID="Freemind_Link_1912187024" MODIFIED="1269767355922" TEXT="http_session_manager:gen_server"/>
+<node CREATED="1269767357794" ID="Freemind_Link_1696399323" MODIFIED="1269767382650" TEXT="auth: gen_server"/>
+</node>
+<node CREATED="1269765860037" ID="Freemind_Link_1348401077" MODIFIED="1269766233088" TEXT="TestCase">
+<node CREATED="1269766234564" ID="Freemind_Link_152386539" MODIFIED="1269767044509" TEXT="&#x421;&#x43e;&#x437;&#x434;&#x430;&#x43d;&#x438;&#x435; http session"/>
+<node CREATED="1269766255893" ID="Freemind_Link_1207074429" MODIFIED="1269766273128" TEXT="login"/>
+<node CREATED="1269766708557" ID="Freemind_Link_200863916" MODIFIED="1269766713661" TEXT="&#x418;&#x433;&#x440;&#x43e;&#x432;&#x430;&#x44f; &#x430;&#x43a;&#x442;&#x438;&#x432;&#x43d;&#x43e;&#x441;&#x442;&#x44c;">
+<node CREATED="1269767792504" ID="Freemind_Link_963386416" MODIFIED="1269767856209" TEXT="&#x421;&#x43e;&#x437;&#x434;&#x430;&#x43d;&#x438;&#x435; &#x43f;&#x435;&#x440;&#x441;&#x43e;&#x43d;&#x430;&#x436;&#x430;"/>
+<node CREATED="1269767805942" ID="Freemind_Link_1263023975" MODIFIED="1269767865432" TEXT="&#x41f;&#x440;&#x438;&#x441;&#x43e;&#x435;&#x434;&#x438;&#x43d;&#x435;&#x43d;&#x438;&#x435; &#x43a; &#x438;&#x433;&#x440;&#x435;"/>
+<node CREATED="1269767910059" ID="Freemind_Link_501911016" MODIFIED="1269767918349" TEXT="&#x41e;&#x442;&#x441;&#x44b;&#x43b;&#x43a;&#x430; &#x435;&#x432;&#x435;&#x43d;&#x442;&#x43e;&#x432; &#x43d;&#x430; &#x43a;&#x43b;&#x438;&#x435;&#x43d;&#x442;&#x435;">
+<node CREATED="1269767919298" ID="Freemind_Link_419573868" MODIFIED="1269767923166" TEXT="&#x427;&#x435;&#x440;&#x435;&#x437; long polling"/>
+<node CREATED="1269767924349" ID="Freemind_Link_408715558" MODIFIED="1269767927869" TEXT="&#x41a;&#x43b;&#x438;&#x435;&#x43d;&#x442; &#x43f;&#x43e; &#x442;&#x430;&#x439;&#x43c;&#x435;&#x440;&#x443;"/>
+</node>
+<node CREATED="1269768000301" ID="Freemind_Link_500504247" MODIFIED="1269768030063" TEXT="&#x418;&#x433;&#x440;&#x43e;&#x432;&#x430;&#x44f; &#x441;&#x435;&#x441;&#x441;&#x438;&#x44f;">
+<node CREATED="1269768030941" ID="Freemind_Link_1623871994" MODIFIED="1269768034216" TEXT="&#x41d;&#x430;&#x437;&#x43d;&#x430;&#x447;&#x435;&#x43d;&#x438;&#x435;">
+<node CREATED="1269768035139" ID="Freemind_Link_97674917" MODIFIED="1269768040599" TEXT="&#x410;&#x431;&#x441;&#x442;&#x440;&#x430;&#x433;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435; &#x43e;&#x442; &#x442;&#x440;&#x430;&#x43d;&#x441;&#x43f;&#x43e;&#x440;&#x442;&#x430;"/>
+<node CREATED="1269768041651" ID="Freemind_Link_887620350" MODIFIED="1269768050176" TEXT="&#x421;&#x438;&#x43d;&#x445;&#x440;&#x43e;&#x43d;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f; &#x43f;&#x43e; &#x438;&#x433;&#x440;&#x43e;&#x43a;&#x443;"/>
+</node>
+<node CREATED="1269768058018" ID="Freemind_Link_1548945080" MODIFIED="1269768063054" TEXT="&#x420;&#x435;&#x430;&#x43b;&#x438;&#x437;&#x430;&#x446;&#x438;&#x44f;">
+<node CREATED="1269768064130" ID="Freemind_Link_1478540431" MODIFIED="1269768066445" TEXT="gen:server"/>
+</node>
+</node>
+</node>
+<node CREATED="1269766266219" ID="Freemind_Link_1458207570" MODIFIED="1269766275086" TEXT="logout"/>
+<node CREATED="1269766716501" ID="Freemind_Link_316455355" MODIFIED="1269766725607" TEXT="logout by timeout"/>
+</node>
+<node CREATED="1269769328871" ID="Freemind_Link_1198051410" MODIFIED="1269769337777" TEXT="GameManager">
+<node CREATED="1269769383519" ID="Freemind_Link_606465322" MODIFIED="1269776469881" TEXT="create(playerId):bool"/>
+<node CREATED="1269769386875" ID="Freemind_Link_1576132019" MODIFIED="1269769454676" TEXT="join(gameId, playerId):bool">
+<node CREATED="1269769679771" ID="Freemind_Link_212942442" MODIFIED="1269769685290" TEXT="&#x438;&#x437;&#x43c;&#x435;&#x43d;&#x44f;&#x435;&#x442; GameState"/>
+<node CREATED="1269769687006" ID="Freemind_Link_1617296884" MODIFIED="1269769697622" TEXT="&#x43e;&#x442;&#x441;&#x44b;&#x43b;&#x430;&#x435;&#x442; &#x435;&#x433;&#x43e; &#x43d;&#x430; client(&#x435;&#x441;&#x43b;&#x438; &#x435;&#x441;&#x442;&#x44c; longpolling)">
+<node CREATED="1269769716345" ID="Freemind_Link_1219679745" MODIFIED="1269769743147" TEXT="&#x43a;&#x438;&#x434;&#x430;&#x435;&#x442; event &#x447;&#x435;&#x440;&#x435;&#x437; gen_event"/>
+<node CREATED="1269769744921" ID="Freemind_Link_10623381" MODIFIED="1269769751587" TEXT="GameSession - &#x44d;&#x442;&#x43e; gen_event???"/>
+</node>
+</node>
+<node CREATED="1269769412273" ID="Freemind_Link_1945992778" MODIFIED="1269769466467" TEXT="leave(gameId, playerId):bool"/>
+<node CREATED="1269769469408" ID="Freemind_Link_1563147407" MODIFIED="1269769477158" TEXT="turn(gameId, playerId, data)"/>
+<node CREATED="1269769391420" ID="Freemind_Link_1868832135" MODIFIED="1269769399631" TEXT="GameSession"/>
+<node CREATED="1269769625334" ID="Freemind_Link_908492659" MODIFIED="1269769640798" TEXT="getGameState(playerId):GameState"/>
+<node CREATED="1269776083858" ID="Freemind_Link_91676860" MODIFIED="1269776085447" TEXT="timeout"/>
 </node>
 </node>
 </node>
